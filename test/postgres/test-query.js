1baa4ef13d5ed2638cf1a29cd6215457b89e9792
Tim Caswell <tim@creationix.com>
2009-12-15 20:23:10 -0600

    Make postgres's query pass the tests.

--- test/postgres/test-query.js
+++ test/postgres/test-query.js
@@ -8,17 +8,21 @@ var finished2 = false;
 var finished3 = false;
 var finished4 = false;
 
-db.query("SELECT null AS test1, 2 AS test2, 'hello' AS test3").addCallback(function (data) {
+db.query("SELECT null AS test1, 2 AS test2, 'hello' AS test3, false AS test4, true AS test5").addCallback(function (data) {
   assert.equal(data[0].test1, null);
-  assert.equal(data[0].test2, '2');
+  assert.equal(data[0].test2, 2);
   assert.equal(data[0].test3, 'hello');
+  assert.equal(data[0].test4, false);
+  assert.equal(data[0].test5, true);
   finished1 = true;
 })
 
-db.query("SELECT null AS test1, 2 AS test2, 'hello' AS test3", function (row) {
+db.query("SELECT null AS test1, 2 AS test2, 'hello' AS test3, false AS test4, true AS test5", function (row) {
   assert.equal(row.test1, null);
-  assert.equal(row.test2, '2');
+  assert.equal(row.test2, 2);
   assert.equal(row.test3, 'hello');
+  assert.equal(row.test4, false);
+  assert.equal(row.test5, true);
   finished2 = true;
 }).addCallback(function () {
   assert.equal(arguments.length, 0, "Streaming queries shouldn't return buffered results");
